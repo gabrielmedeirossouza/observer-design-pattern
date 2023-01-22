c9201d018c21f761196381fca42453ae12f00610
Gabriel Medeiros Souza <gabriel@Nabucodonosor>
2023-01-22 13:57:59 -0300

    refactor: change observer-factory from function to class

--- src/factories/observer-factory/observer-factory.ts
+++ src/factories/observer-factory/observer-factory.ts
@@ -1,13 +1,9 @@
 import { Observer } from "../../entities/observer";
 
-export function ObserverFactory<K extends { [key: string]: Function }>() {
-  function createObserver<A extends keyof K & string>(event: A, callback: K[A]) {
+export class ObserverFactory<K extends { [key: string]: Function }> {
+  public CreateObserver<A extends keyof K & string>(event: A, callback: K[A]) {
     const observer = new Observer<A, K[A]>(event, callback);
 
     return observer;
   }
-
-  return {
-    createObserver
-  };
 }
